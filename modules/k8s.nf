e702f770258fc1ec3bab5a0e460c6666437ec15f
Alexander Shires <ashires@google.com>
2022-02-02 15:59:34 +0100

    update

--- modules/k8s.nf
+++ modules/k8s.nf
@@ -8,6 +8,6 @@ process k8s_step {
     executor = 'k8s'
     container = 'eu.gcr.io/ashires-pso-experimental/nextflow-step-container:latest'
     script: """
-  python3 step.py nextflow-container
+  python3 /app/step.py nextflow-container
   """
 }
--- run-k8s.sh
+++ run-k8s.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-./nextflow -C nextflow.config run alexshires/nextflow-demos -r 23806fc -w gs://ashires-nextflow-work/k8s-test
+./nextflow -C nextflow.config run alexshires/nextflow-demos -r 23806fc -w /mnt/nextflow/default/test -b gs://ashires-nextflow-work/k8s-test
 # -v nextflow-filestore-nextflow-claim:/mnt/nextflow/nextflow
